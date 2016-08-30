1931c093f3c2c703190a7761a19ca982a7cd1432
zhaotao <zhao-tao@msn.com>
2016-08-30 14:55:14 +0800

    提交到本地主分支和线上主分支测试

--- app/src/main/java/zhao/test/com/mytest/MainActivity.java
+++ app/src/main/java/zhao/test/com/mytest/MainActivity.java
@@ -2,25 +2,21 @@ package zhao.test.com.mytest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.ImageView;
+import android.widget.Button;
 import android.widget.TextView;
 
-import com.android.volley.RequestQueue;
-
 public class MainActivity extends AppCompatActivity {
-    private final static String Tag = "MainActivity";
     private String requestInfo;
-    private TextView tvInfo;
-    private RequestQueue requestQueue;
-    private ImageView ivTest;
+    private TextView tvQuestion;
+    private Button btnTrue;
+    private Button btnFalse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i(Tag, "onCreate");
-        tvInfo = (TextView) findViewById(R.id.tv_info);
-        ivTest = (ImageView) findViewById(R.id.iv_test);
+        tvQuestion = (TextView) findViewById(R.id.tv_question_info);
+        btnTrue = (Button) findViewById(R.id.btn_true);
+        btnFalse = (Button) findViewById(R.id.btn_false);
     }
 }
